47f37fefc133cb30091663224655428b9d7325ca
REYNOLDS <4800556183@davistech.edu>
2022-02-23 09:50:13 -0700

    mid-game update

---
+++ Assets/FinishLine.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinishLine : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.tag == "Player") Debug.Log("You finished!");
+    }
+}
