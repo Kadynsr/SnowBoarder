02bcb50631022d27c020e149378d039a092464d9
REYNOLDS <4800556183@davistech.edu>
2022-02-23 13:13:01 -0700

    Finished

--- Assets/CrashDetector.cs
+++ Assets/CrashDetector.cs
@@ -1,10 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CrashDetector : MonoBehaviour
 {
+    [SerializeField] float invokedDelay = .5f;
+    [SerializeField] ParticleSystem crashEffect;
+    [SerializeField] AudioClip crashSFX;
+    bool alreadyHit = false;
     void OnTriggerEnter2D(Collider2D collision) {
-        if (collision.tag == "Ground") Debug.Log("You hit your head!");
+        if (collision.tag == "Ground" && alreadyHit is false) {
+            alreadyHit = true;
+            FindObjectOfType<PlayerController>().DisableControls();
+            Debug.Log("You hit your head!");
+            crashEffect.Play();
+            GetComponent<AudioSource>().PlayOneShot(crashSFX);
+            Invoke("ReloadScene", invokedDelay);
+        }
+    }
+    void ReloadScene() {
+        SceneManager.LoadScene(0);
     }
 }
--- Assets/FinishLine.cs
+++ Assets/FinishLine.cs
@@ -1,10 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class FinishLine : MonoBehaviour
 {
+    [SerializeField] float invokedDelay = 1f;
+    [SerializeField] ParticleSystem finishEffect;
+
     void OnTriggerEnter2D(Collider2D collision) {
-        if (collision.tag == "Player") Debug.Log("You finished!");
+        if (collision.tag == "Player") {
+            Debug.Log("You finished!");
+            finishEffect.Play();
+            GetComponent<AudioSource>().Play();
+            Invoke("ReloadScene", invokedDelay);
+        }
+    }
+
+    void ReloadScene() {
+        SceneManager.LoadScene(0);
     }
 }
